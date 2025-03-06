bf1b2a333dcacacf54d9d36252196b182ee6001e
ahmedfehor <ahmedfehor@gmail.com>
2025-03-07 01:53:46 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+
+    // $loggedInUser = session('appuser');
+    // dd($loggedInUser);
+    return redirect()->route('chats');
+    // return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return redirect()->route('chats');
+    // return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Route::get('/reverb', function () {
+//     Artisan::call('reverb:start');
+// });
+
+require __DIR__.'/auth.php';
+
+
